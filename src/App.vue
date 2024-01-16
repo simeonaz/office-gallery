22b2fb784590b39d5ea04c1ab64a0cc4e8b46a9f
Simeon A <simeonazogbonon@gmail.com>
2024-01-16 17:22:16 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="grid place-items-center max-w-screen min-h-screen bg-[#f8f8fb]">
+    <div class="flex gap-4 max-w-7xl">
+      <figure class="card relative left-0 w-32 rounded-lg h-96 overflow-hidden shadow-2xl transition-all duration-400 ease-in-out hover:font-bold hover:cursor-pointer">
+        <img src="./images/office 1.jpeg" alt="Daenerys Targaryen" class="relative w-full h-full object-cover">
+      </figure>
+      <figure class="card relative left-0 w-32 rounded-lg h-96 overflow-hidden shadow-2xl transition-all duration-400 ease-in-out hover:font-bold hover:cursor-pointer">
+        <img src="./images/office 2.jpeg" alt="Jon Snow" class="relative w-full h-full object-cover">
+      </figure>
+      <figure class="card relative left-0 w-32 rounded-lg h-96 overflow-hidden shadow-2xl transition-all duration-400 ease-in-out hover:font-bold hover:cursor-pointer">
+        <img src="./images/office 3.jpeg" alt="Tyrion Lannister" class="relative w-full h-full object-cover">
+      </figure>
+      <figure class="card relative left-0 w-32 rounded-lg h-96 overflow-hidden shadow-2xl transition-all duration-400 ease-in-out hover:font-bold hover:cursor-pointer">
+        <img src="./images/office 4.jpeg" alt="Cersei Lannister" class="relative w-full h-full object-cover">
+      </figure>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.card {
+  flex: 0.25;
+}
+
+.card:hover {
+    flex: 2;
+}
+</style>
